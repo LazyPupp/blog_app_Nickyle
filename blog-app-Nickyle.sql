38f9a90aecc1909a980b3c89ddf48b712276ac77
Kyle Szeto <kyl3sz3t0@gmail.com>
2017-05-30 14:52:49 -0700

    fixed the inner join error

--- blog-app-Nickyle.sql
+++ blog-app-Nickyle.sql
@@ -49,18 +49,22 @@ insert into tag_posts(tag_id, post_id) VALUES (1,2);
 -- SELECT * FROM tags;
 -- SELECT * FROM tag_posts;
 
+-- THIS GIVES BACK THE email, sn of the author of the posts whose id is 1
 -- SELECT email, sn , posts.title, posts.content 
 -- FROM user_info INNER JOIN posts ON posts.author_id = user_info.id
 -- WHERE user_info.id = 1; 
 
-select sn AS "comment author", posts.title, posts.content, comments.comment
-FROM user_info INNER JOIN comments ON comments.author_id = user_info.id
-INNER JOIN posts ON comments.posts_id = posts.id
-WHERE sn = 'LoL'
-;
+-- THIS GIVES BACK the comments written by 'LoL' , and which comments relate to which posts
+-- select sn AS "comment author", posts.title, posts.content, comments.comment
+-- FROM user_info INNER JOIN comments ON comments.author_id = user_info.id
+-- INNER JOIN posts ON comments.posts_id = posts.id
+-- WHERE sn = 'LoL'
+-- ;
 
-select postAuthor.sn AS "post author", posts.title, posts.content,commentAuthor.sn AS "comment author", comments.comment
-FROM user_info cA,user_info postAuthor INNER JOIN comments ON comments.author_id = cA.id
-INNER JOIN posts ON comments.posts_id = posts.id
-INNER JOIN posts ON postAuthor.id = posts.id
-WHERE sn = 'LoL';
+-- This gives back the comments written by 'LoL', and which comments relate to which posts + post author
+-- select pA.sn AS "post author", p.title, p.content, cA.sn AS "comment author", c.comment
+-- FROM user_info cA 
+-- INNER JOIN comments c ON c.author_id = cA.id
+-- INNER JOIN posts p ON c.posts_id = p.id
+-- INNER JOIN user_info pA ON pA.id = p.id
+-- WHERE cA.sn = 'LoL';
