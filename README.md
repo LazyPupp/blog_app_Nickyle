01d3ee5c347fab53257c37ef7cd9f092417ce4ad
Kyle Szeto <kyl3sz3t0@gmail.com>
2017-05-30 14:04:23 -0700

    messing around

---
+++ README.md
@@ -0,0 +1,2 @@
+psql -f ~/Desktop/Thinkful/Week4/Day1/blog-app-Nickyle.sql postgres://umarqack:WYaHeIgEBAETwX_5pmh28k1cYIoQVgj5@stampy.db.elephantsql.com:5432/um
+arqack
--- blog-app-Nickyle.sql
+++ blog-app-Nickyle.sql
@@ -8,17 +8,17 @@ CREATE TABLE user_info(
 
 CREATE TABLE posts(
     id serial primary key,
-    author_id references user_info on DELETE CASCADE,
+    author_id integer references user_info on DELETE CASCADE,
     title text not null,
     content text not null,
     time_pub timestamp default current_timestamp,
-    comments text,
+    comments text
 );
 
 CREATE TABLE comments(
     id serial primary key,
-    author_id references user_info on DELETE CASCADE,
-    posts_id references posts on DELETE CASCADE,
+    author_id integer references user_info on DELETE CASCADE,
+    posts_id integer references posts on DELETE CASCADE,
     comment text
 );
 
@@ -28,8 +28,8 @@ CREATE TABLE tags(
 );
 
 CREATE TABLE tag_posts(
-    tag_id references tags,
-    post_id references posts on DELETE CASCADE
+    tag_id integer references tags,
+    post_id integer references posts on DELETE CASCADE
 );
 
 INSERT INTO user_info(
@@ -37,8 +37,18 @@ INSERT INTO user_info(
 
 insert into posts(author_id, title, content) values (1, 'this is why i am named lolz', 'i am named lol because of the lolz'), (2, 'dooooodBROOOOO', 'dude, bros ftw'), (3, 'server side class 101', 'please look at the slides');
 
-insert into comments (author_id, post_id, comment) values (2, 1, 'dude your post sucks'), (1, 2, 'BRO ITS ME SUP MANG'), (1, 3, 'nah man, lol.'), (3, 2, 'dude, study.');
+insert into comments (author_id, posts_id, comment) values (2, 1, 'dude your post sucks'), (1, 2, 'BRO ITS ME SUP MANG'), (1, 3, 'nah man, lol.'), (3, 2, 'dude, study.');
+
+insert into tags (tag) values ('stuff');
 
 insert into tag_posts(tag_id, post_id) VALUES (1,2);
 
-insert into tags (tag) value('stuff');
+-- SELECT * FROM user_info;
+-- SELECT * FROM posts;
+-- SELECT * FROM comments;
+-- SELECT * FROM tags;
+-- SELECT * FROM tag_posts;
+
+-- SELECT email, sn , posts.title, posts.content 
+-- FROM user_info INNER JOIN posts ON posts.author_id = user_info.id
+-- WHERE user_info.id = 1; 
