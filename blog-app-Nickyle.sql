e59eb49a357ee60d1dd4877efffb89a33cad42b5
nfjones87 <nfjones87@gmail.com>
2017-05-30 13:42:59 -0700

    insert values and reference deletes

--- blog-app-Nickyle.sql
+++ blog-app-Nickyle.sql
@@ -8,21 +8,37 @@ CREATE TABLE user_info(
 
 CREATE TABLE posts(
     id serial primary key,
-    author_id references user_info,
-    content text,
-    time_pub timestamp,
+    author_id references user_info on DELETE CASCADE,
+    title text not null,
+    content text not null,
+    time_pub timestamp default current_timestamp,
     comments text,
-    tags text
 );
 
 CREATE TABLE comments(
     id serial primary key,
-    author_id references user_info,
-    posts_id references posts,
+    author_id references user_info on DELETE CASCADE,
+    posts_id references posts on DELETE CASCADE,
     comment text
 );
-CREATE TABLE blog_posts(
-    comment_id references comments,
-    posts_id references posts,
-    
-)
+
+CREATE TABLE tags(
+    id serial primary key,
+    tag text
+);
+
+CREATE TABLE tag_posts(
+    tag_id references tags,
+    post_id references posts on DELETE CASCADE
+);
+
+INSERT INTO user_info(
+    email, sn) values ('lol@gmail.com', 'LoL'), ('dudebro@aol.com', '1337hax0r'), ('chrisKlanc@hotmail.com', 'proInstruct0r');
+
+insert into posts(author_id, title, content) values (1, 'this is why i am named lolz', 'i am named lol because of the lolz'), (2, 'dooooodBROOOOO', 'dude, bros ftw'), (3, 'server side class 101', 'please look at the slides');
+
+insert into comments (author_id, post_id, comment) values (2, 1, 'dude your post sucks'), (1, 2, 'BRO ITS ME SUP MANG'), (1, 3, 'nah man, lol.'), (3, 2, 'dude, study.');
+
+insert into tag_posts(tag_id, post_id) VALUES (1,2);
+
+insert into tags (tag) value('stuff');
